c9b51f49e8c6fe08ca42f85aacf9055a2e7a38e9
fikrahdamars <fikrahdamar7@gmail.com>
2026-08-19 14:58:07 +0700

    initial project

---
+++ woolone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  woolone
+//
+//  Created by Fikrah Damar Huda on 19/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ woolone/wooloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  wooloneApp.swift
+//  woolone
+//
+//  Created by Fikrah Damar Huda on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct wooloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
